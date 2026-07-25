611058af41b14c5aabae436fc68ad0e875fb30c0
Red <redonx99@gmail.com>
2026-07-25 13:01:46 -0400

    Fix mobile production asset routing

--- mobile/vite.config.js
+++ mobile/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { cloudflare } from '@cloudflare/vite-plugin'
+import { sites } from './build/sites-vite-plugin.js'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sites(),
+    cloudflare({
+      viteEnvironment: { name: 'server' },
+    }),
+  ],
 })
